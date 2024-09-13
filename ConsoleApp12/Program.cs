cc900c16653b3c6461fa28676eb771e15f98addd
Your Name <you@example.com>
2024-09-13 17:53:20 +0700

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhập cạnh đáy lớn (a):");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("nhập cạnh đáy nhỏ (b):");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("nhập chiều cao (h):");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            double dientich = (a + b) * h / 2;
+
+            Console.WriteLine($"diện tích hình thang là :{dientich}");
+
+        }
+    }
+}
---
+++ lib_toan/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lib_toan
+{
+    public class Class1
+    {
+        public static double CalculaateAre(double a, double b, double h)
+        {
+           return (a +b)*h/2;
+        }
+    }
+}
