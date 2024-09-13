a2c79dbb5f1be9a08b94b1c0f2116e32b9d688cd
Your Name <you@example.com>
2024-09-13 17:56:22 +0700

    đã làm code được bài toán, biết sử dụng dll

--- lib_toan/Class1.cs
+++ lib_toan/Class1.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace lib_toan
 {
     public class Class1
-    {
+    {// công thích tính
         public static double CalculaateAre(double a, double b, double h)
         {
            return (a +b)*h/2;
